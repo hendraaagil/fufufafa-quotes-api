91198362b934bb892877475a4cdb5768d6034fed
hendraaagil <hendraaagil@gmail.com>
2025-04-10 10:14:56 +0700

    feat: add api routes to get quote data

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,36 @@
 import { Hono } from 'hono'
+import { generateRandomInteger } from './libs/generator'
+import quotes from './data/quotes.json'
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.json({
+    routes: [
+      {
+        path: '/quote',
+        description: 'Get a random quote',
+      },
+      {
+        path: '/list',
+        description: 'Get a list of all quotes',
+      },
+    ],
+    author: {
+      name: 'Hendra Agil',
+      site: 'https://hendraaagil.dev',
+    },
+    source: 'https://github.com/hendraaagil/...',
+  })
+})
+
+app.get('/quote', (c) => {
+  const quote = quotes[generateRandomInteger(0, quotes.length - 1)]
+  return c.json(quote)
+})
+
+app.get('/list', (c) => {
+  return c.json(quotes)
 })
 
 export default app
